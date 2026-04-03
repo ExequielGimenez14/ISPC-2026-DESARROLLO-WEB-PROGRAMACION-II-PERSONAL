c3d1e3fde856bf773cc946c1ceb21dd2fb10c041
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2026-04-03 20:41:54 -0300

    Integracion de Django con MySQL

--- ejercicioUser/my_project/settings.py
+++ ejercicioUser/my_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appdjango',
+        'USER': 'root',
+        'PASSWORD': 'Dragonballz11',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
