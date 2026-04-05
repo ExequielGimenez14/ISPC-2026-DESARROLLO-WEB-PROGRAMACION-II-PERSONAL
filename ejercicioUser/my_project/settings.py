4af281b1a6d75d76a08d45e08fbdb8832eb378c5
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2026-04-05 20:14:53 -0300

    Conexion de Django con DB SQLite

--- ejercicioUser/my_project/settings.py
+++ ejercicioUser/my_project/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'appdjango',
-        'USER': 'root',
-        'PASSWORD': 'Dragonballz11',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
